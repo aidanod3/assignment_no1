f5199bf9cb10b731bda728d9da3d3a314c28f0fe
aidan <aidanod3@gmail.com>
2025-01-27 13:47:47 -0500

    added weird indentations

---
+++ src/test.java
@@ -0,0 +1,11 @@
+public class test {
+
+    public static void main(String[] args) {
+                    System.out.println("hello world");
+                              //funny indentation lmaoo
+
+        //test
+                    //test
+        System.out.println("test");
+    }
+}
