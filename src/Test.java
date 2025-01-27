36b867dfaa6e8c9056846d958ea58d779537fbde
aidan <aidanod3@gmail.com>
2025-01-27 13:53:15 -0500

    uppercased file name

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,8 @@
-public class test {
+public class Test {
 
     public static void main(String[] args) {
         System.out.println("hello world");
         //funny indentation lmaoo
-
         //test
         // test
         System.out.println("test");
